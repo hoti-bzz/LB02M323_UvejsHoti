8ec27f4a94e5b3ae00f98941ac44cdda43ff09f2
uvejs <hotiu@bzz.ch>
2024-11-08 08:58:49 +0100

    done

--- userBlueprint.py
+++ userBlueprint.py
@@ -7,6 +7,7 @@ user_blueprint = Blueprint('users', __name__)
 # Global users list
 users = []
 
+
 @user_blueprint.route('/register', methods=['POST'])
 def register_user():
     data = request.get_json()
@@ -14,6 +15,7 @@ def register_user():
     password = data.get("password")
     users.append({"username": username, "password": password})
     return jsonify({"message": f"User {username} successfully registered."})
+
 
 @user_blueprint.route('/login', methods=['POST'])
 def login_user():
@@ -25,6 +27,7 @@ def login_user():
             return jsonify({"message": f"Welcome back, {username}!"})
     return jsonify({"message": "Login failed. Please check your credentials."}), 401
 
+
 @user_blueprint.route('/list', methods=['GET'])
 def list_users():
     if not users:
@@ -34,8 +37,11 @@ def list_users():
     ]
     return jsonify(formatted_users)
 
+
 def user_review_summary(filtered_books):
-    users_with_reviews = list(filter(lambda user: any(user['username'] == r['username'] for book in filtered_books for r in book['reviews']), users))
+    users_with_reviews = list(
+        filter(lambda user: any(user['username'] == r['username'] for book in filtered_books for r in book['reviews']),
+               users))
     usernames = list(map(lambda user: user['username'], users_with_reviews))
     review_count = reduce(lambda acc, user: acc + sum(1 for book in filtered_books for r in book['reviews']
                                                       if r['username'] == user['username']), users_with_reviews, 0)
